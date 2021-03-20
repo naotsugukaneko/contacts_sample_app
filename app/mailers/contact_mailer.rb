58c11f190dbaf37eb20e4ea87e4790211b5182b6
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-03-20 14:10:07 +0000

    お問い合わせ用のメイラーを設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,14 +1,20 @@
 class ContactMailer < ApplicationMailer
   
-  def user_email(name:, email:)
-    @name = name
-    mail(
-      to: email,
-      subject: "【お問い合わせアプリ】お問い合わせを受付いたしました"
-    )
+  ADMIN_EMAIL = "sample.test.812@gmail.com"
+  
+  def user_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【お問い合わせアプリ】問い合わせを受付いたしました"
+    
+    mail(to: contact.email, subject: subject)
   end
 
-  def admin_email
+  def admin_email(contact)
+    @contact = contact
+    @name = contact.name.present? ? contact.name : contact.email
+    subject = "【お問い合わせアプリ】問い合わせがありました"
     
+    mail(to: ADMIN_EMAIL, subject: subject)
   end
 end
